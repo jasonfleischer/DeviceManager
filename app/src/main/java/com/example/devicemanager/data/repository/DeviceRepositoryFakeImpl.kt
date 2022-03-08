98ea08083fad4ad08364dd269c6db2b46602495d
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-03-08 16:44:51 -0500

    update

---
+++ app/src/main/java/com/example/devicemanager/data/repository/DeviceRepositoryFakeImpl.kt
@@ -0,0 +1,82 @@
+package com.example.devicemanager.data.repository
+
+import com.example.devicemanager.domain.model.Device
+import com.example.devicemanager.domain.repository.DeviceRepository
+
+class DeviceRepositoryFakeImpl : DeviceRepository {
+    override suspend fun getDevices(): List<Device> {
+        return listOf(
+            Device(
+                "1",
+                "Samsung Galaxy Phone",
+                "Phone",
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                90,
+                "CAD",
+                true,
+                "https://jasonfleischer.github.io/website/imgs/hcn_tablet.jpg"
+            ),
+            Device(
+                "2",
+                "iPhone",
+                "Phone",
+                "Perspiciatis spernatur.",
+                190,
+                "USD",
+                false,
+                ""
+            ),
+            Device(
+                "3",
+                "Apple macbook",
+                "Computer",
+                "",
+                132,
+                "USD",
+                false,
+                ""
+            ),
+            Device(
+                "4",
+                "",
+                "",
+                "",
+                0,
+                "",
+                false,
+                ""
+            ),
+            Device(
+                "5",
+                "1 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                "2 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                "3 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                0,
+                "4 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+                false,
+                "5 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            ),
+            Device(
+                "4",
+                "",
+                "",
+                "",
+                0,
+                "",
+                false,
+                ""
+            ),
+            Device(
+                "4",
+                "     ",
+                "  ",
+                "    ",
+                0,
+                "   ",
+                false,
+                "  "
+            )
+
+        )
+    }
+}
--- app/src/main/java/com/example/devicemanager/dependency_injection/AppModule.kt
+++ app/src/main/java/com/example/devicemanager/dependency_injection/AppModule.kt
@@ -2,7 +2,7 @@ package com.example.devicemanager.dependency_injection
 
 import com.example.devicemanager.common.Constants
 import com.example.devicemanager.data.remote.DeviceManagerApi
-import com.example.devicemanager.data.repository.DeviceRepositoryImpl
+import com.example.devicemanager.data.repository.DeviceRepositoryFakeImpl
 import com.example.devicemanager.domain.repository.DeviceRepository
 import dagger.Module
 import dagger.Provides
@@ -29,6 +29,7 @@ object AppModule {
     @Provides
     @Singleton
     fun provideDeviceRepository(api: DeviceManagerApi): DeviceRepository {
-        return DeviceRepositoryImpl(api)
+        //return DeviceRepositoryImpl(api)
+        return DeviceRepositoryFakeImpl()
     }
 }
